e0c81d1df8e38989c372eb7d7ef68917cf90ff8c
TaufiqWahyu <taufiqwahyu03@gmail.com>
2025-06-17 09:57:13 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -2,14 +2,12 @@ package hitung;
 
 public class MatematikaBeraksi {
     public static void main(String[] args){
-        Matematika mtk = new Matematika();
+        //membuat objek
+        Matematika taufiq = new Matematika(9,2);
         
-        int a = 20;
-        int b = 4;
-        
-        System.out.println("Penjumlahan: "+ mtk.tambah(a, b));
-        System.out.println("Pengurangan: "+ mtk.kurang(a, b));
-        System.out.println("Perkalian: "+ mtk.kali(a, b));
-        System.out.println("Pembagian: "+ mtk.bagi(a, b));
+        System.out.println("Hasil Penjumlahan: " + taufiq.setPenjumlahan());
+        System.out.println("Hasil Pengurangan: " + taufiq.setPengurangan());
+        System.out.println("Hasil Perkalian: " + taufiq.setPerkalian());
+        System.out.println("Hasil Pembagian: " + taufiq.setPembagian());
     }
 }
